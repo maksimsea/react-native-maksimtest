3b88812a957ed76932e3ccdf32fef812a2e2e651
maksimsea <maksimsea@gmail.com>
2023-01-11 13:15:00 +0300

    dondone

--- __ios/Maksimtest.swift
+++
@@ -1,9 +0,0 @@
-@objc(Maksimtest)
-class Maksimtest: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    //print("123123123");
-    resolve(a*b)
-  }
-}
